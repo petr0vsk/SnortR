8e951ced62465af2ecf001208799a126558bdf47
petr0vsk <petr0vskjy.aleksander@gmail.com>
2018-11-13 05:07:44 -0500

    find duplicate error

--- time_series_regularization.R
+++ time_series_regularization.R
@@ -29,7 +29,7 @@ sec_interval <- zoo(, seq(start(irreg.times), end(irreg.times), "sec"))
 # и наконец объеденим две шкалы, приводя выборку к регулярной. пропуски по времени выборки
 # заполняем нулями
 reg.times.agg <- merge(irreg.times.agg, sec_interval, fill = 0)
-autoplot.zoo(reg.times.agg) + ggtitle("Распределение по времени длины фреймов канального уровня") + geom_area(alpha = 0.15) + geom_point() + geom_smooth(method = "loess", se = FALSE) + labs(x = "Время", y = "Суммарна длина пакетов/секунду")
+autoplot.zoo(reg.times.agg) + ggtitle("Распределение по времени длины фреймов канального уровня") + geom_line(alpha = 0.15) + geom_point() + geom_smooth(method = "loess", se = FALSE) + labs(x = "Время", y = "Суммарна длина пакетов/секунду")
 #########################################################################
 # суммируем длину пакетов протокола TCP внутри окна в одну секунду
 #############################################################################
@@ -47,8 +47,24 @@ sec_interval <- zoo(, seq(start(irreg.times), end(irreg.times), "sec"))
 reg.times.agg <- merge(irreg.times.agg, sec_interval, fill = 0)
 autoplot.zoo(reg.times.agg) + ggtitle("Распределение по времени длины пакета TCP IP") + geom_area(alpha = 0.15) + geom_point() + geom_smooth(method = "loess", se = FALSE) + labs(x = "Время", y = "Суммарна длина пакетов/секунду")
 
-
-
+########################################################################################
+# функция вывода графика количественного параметра трафика (длины пакета соответствующего уровня модели TCP IP, кол-ва пакетов 
+# с определенным флагом и т.п.). В качестве параметров принимает следующие значения:
+# field - количественный параметр трафика
+# interval - интервал на котором проводится суммирование (по умолчанию секунды)
+#######################################################################################
+plot_time_series <- function(field, interval) {
+  # создадим временную серию класса zoo
+  irreg.times <- zoo(field, order.by = time.t )
+  # суммируем длину пакетов TCP внутри временного окна 
+  irreg.times.agg  <- aggregate(irreg.times, as.POSIXct(trunc(time(irreg.times), "sec")), sum)
+  # теперь вычисляем шкалу времени без пропусков от начальной до конечной точки выборки
+  sec_interval <- zoo(, seq(start(irreg.times), end(irreg.times), "sec"))
+  # и наконец объеденим две шкалы, приводя выборку к регулярной. пропуски по времени выборки
+  # заполняем нулями
+  eg.times.agg <- merge(irreg.times.agg, sec_interval, fill = 0)
+  autoplot.zoo(reg.times.agg) + ggtitle("Распределение по времени") + geom_area(alpha = 0.15) + geom_point() + geom_smooth(method = "loess", se = FALSE) + labs(x = "Время", y = "Суммарна длина пакетов/секунду")
+}
 
 
 
