56174361ab7a5a5444b33d7b2d12aa43e5ff9d2b
petr0vsk <petr0vskjy.aleksander@gmail.com>
2018-11-13 02:38:57 -0500

    add read file to timeseries

--- time_series_regularization.R
+++ time_series_regularization.R
@@ -5,20 +5,28 @@
 # значение_переменной = 0
 ##########################################################################################
 
+rm(list=ls())
 library(zoo)
 library(ggplot2)
 library(forecast)
+##----------------------------------------------------------------------------------##
+# read pcap
+setwd('WorkR/SnortR/SnortR/TrafficExamples')
+#read dataset
+#dataset <- read.table("scan_tcp-1.txt", quote="\"", header = T, sep=",")
+dataset <- read.table("CTF_1.txt", quote="\"", header = T, sep=",") %>%
+na.omit(dataset)
 #-# tcp_streams <- tcp_streams[c(1:10), ] # временно укоротим выборку для скорости макетирования
-time <- as.character.Date(tcp_streams$frame.time)
+time <- as.character.Date(dataset$frame.time)
 # преобразуем timestamp к виду пригодному для обработки
 time.t <- strptime(time, '%b %d, %Y %H:%M:%S')
 # создадим временную серию класса zoo
-irreg.times <- zoo(tcp_streams$frame.len, order.by = time.t )
-# суммируем длину пакетов внутри окна в одну секунду
+irreg.times <- zoo(dataset$frame.len, order.by = time.t )
+# суммируем длину пакетов канального уровня внутри окна в одну секунду
 irreg.times.agg  <- aggregate(irreg.times, as.POSIXct(trunc(time(irreg.times), "sec")), sum)
 # теперь вычисляем шкалу секунд без пропусков от начальной до конечной секунды выборки 
 sec_interval <- zoo(, seq(start(irreg.times), end(irreg.times), "sec"))
 # и наконец объеденим две шкалы, приводя выборку к регулярной. пропуски по времени выборки
 # заполняем нулями
 reg.times.agg <- merge(irreg.times.agg, sec_interval, fill = 0)
-autoplot.zoo(reg.times.agg) + ggtitle("Общая длина пакетов канального уровня") + geom_area(alpha = 0.15) + geom_point() + geom_smooth(method = "loess", se = FALSE) + labs(x = "Время", y = "Суммарна длина пакетов/секунду")
+autoplot.zoo(reg.times.agg) + ggtitle("Распределение по времени длины фреймов канального уровня") + geom_area(alpha = 0.15) + geom_point() + geom_smooth(method = "loess", se = FALSE) + labs(x = "Время", y = "Суммарна длина пакетов/секунду")
