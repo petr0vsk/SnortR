35b92c3a1a88ee48f3bb2d1093219191f2f94cb8
petr0vsk <petr0vskjy.aleksander@gmail.com>
2019-03-02 15:19:23 +0300

    начали делать gui

---
+++ gui/global.R
@@ -0,0 +1,15 @@
+rm(list = ls())
+
+library(stringr)
+library(tidyr)
+library(dplyr)
+
+library(ggplot2)
+library(igraph)
+
+
+library(shiny)
+library(shinydashboard)
+#source("header.R")
+#source("sidebar.R")
+#source("body.R")
