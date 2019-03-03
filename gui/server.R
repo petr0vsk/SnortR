fdfacbd1607b5edad1b53647cf4cfcd29c0bb5b5
Aleksander Petrovskii <petr0vskjy.aleksander@gmail.com>
2019-03-03 18:38:37 +0300

    add load file in gui

--- gui/server.R
+++ gui/server.R
@@ -1,16 +1,44 @@
-#  Здесь будет наш сервер
 server <- function(input, output) {
   
-  
-  output$distPlot <- renderPlot({
+  output$contents <- DT::renderDT({
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+   #(df[, input$show_vars, drop = FALSE])
     
-  })
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    output$value <- renderPrint({
+        req(input$file1)
+        str(output$contents)
+    })
+   
+    
+    
+    
+  }) #--renderDT(..
   
 }
--- gui/ui.R
+++ gui/ui.R
@@ -1,30 +1,76 @@
-# Вьюшка  ----
 ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
+  tags$head(
+    # подключим свою таблицу стилей
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
+  ui <- fluidPage(
+    # Заголовок
+    titlePanel("Анализ и визуализация параметров трафика TCP/IP"),
+    # Боковая панель для выбора файла для анализа
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("file1", "Выбери CSV или TXT файл",
+                  multiple = FALSE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")),
+        # Ввод: если файл имеет заголовок ----
+        checkboxInput("header", "Header", TRUE),
+        
+        # Ввод: выбор разделителя ----
+        radioButtons("sep", "Separator",
+                     choices = c(Comma = ",",
+                                 Semicolon = ";",
+                                 Tab = "\t"),
+                     selected = ","),
+        ###
+        # Ввод выбор переноса ----
+        radioButtons("quote", "Quote",
+                     choices = c(None = "",
+                                 "Double Quote" = '"',
+                                 "Single Quote" = "'"),
+                     selected = '"'),
+        # Вывод: всю талбилцу или только head ----
+        radioButtons("disp", "Display",
+                     choices = c(Head = "head",
+                                 All = "all"),
+                     selected = "all")
+      ),
+      #
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Общая статитстика", helpText("Здесь отображается содержимое трафика"),
+                                       h3("Трафик"),
+                                       # вывод файла/таблицы с трафиком
+                                       #tableOutput("contents")
+                                       DT::DTOutput("contents")
+                                       ),
+          tabPanel("Plot 2", helpText("Note2: help text isn't a true widget,", 
+                                      "but it provides an easy way to add text to",
+                                      "accompany other widgets.Note: help text isn't a true widget,", 
+                                      "but it provides an easy way to add text to",
+                                      "accompany other widgets.Note: help text isn't a true widget,", 
+                                      "but it provides an easy way to add text to",
+                                      "accompany other widgets.")),
+          tabPanel("Plot 3", helpText("Note3: help text isn't a true widget,", 
+                                      "but it provides an easy way to add text to",
+                                      "accompany other widgets.Note: help text isn't a true widget,", 
+                                      "but it provides an easy way to add text to",
+                                      "accompany other widgets.Note: help text isn't a true widget,", 
+                                      "but it provides an easy way to add text to",
+                                      "accompany other widgets."))
+        ))
       
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
+        
+      ) #  mainPanel
     ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
-  )
-)
+  fluidRow(
+    column(12, 
+           h3("Что-то еще на всю ширину")
+           # вывод файла/таблицы с трафиком
+           #tableOutput("contents")
+           #DT::DTOutput("contents")
+    ))
+  )#
+
+
